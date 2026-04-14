5f69e74865ee0b318dc5d4d8fcdf774bf8aa1b27
FantasticMachine <kotlik.vojtech@juniorlondynska.cz>
2026-04-14 11:45:01 +0200

    Files can now be opened - the insides are loaded and presented in the text area

--- src/Function_File.java
+++ src/Function_File.java
@@ -1,7 +1,10 @@
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Function_File {
     GUI gui; // declare the GUI class to use methods and variables
+    String filename, fileAddress;
 
     // constructor
     public Function_File(GUI gui) {
@@ -18,5 +21,31 @@ public class Function_File {
     public void open() {
         FileDialog fd = new FileDialog(gui.window, "Open file", FileDialog.LOAD); // creating the new window
         fd.setVisible(true); // make it visible
+
+        // loading the file - name and address
+        if (fd.getFile() != null ) {
+            filename = fd.getFile();
+            fileAddress = fd.getDirectory();
+
+            gui.window.setTitle(filename);
+        }
+
+        // loading the file - buffered reader (so the inside)
+        try {
+            BufferedReader br = new BufferedReader( new FileReader(fileAddress + filename) ); // reading the file by taking the address and selecting from there with the filename
+
+            gui.textArea.setText(null);
+
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                gui.textArea.append(line + "\n");
+            }
+
+            br.close();
+        }
+        catch (Exception e) {
+            System.out.println("Error when opening a file ... " + e);
+        }
     }
 }
